e06059f68eb776f4dee95a827e3b7abffce52702
sarahsga <sarahsga4@gmail.com>
2019-09-24 16:23:46 +0500

    Updates

---
+++ precache-manifest.e32b309003ba92c96fec72362befe102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d9d26f7aa05275fe8b1b660ea5e453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f28ce0705542b6e66f",
+    "url": "/static/css/main.42903b45.chunk.css"
+  },
+  {
+    "revision": "b2f55831e3543acf042c",
+    "url": "/static/js/2.037b4350.chunk.js"
+  },
+  {
+    "revision": "85f28ce0705542b6e66f",
+    "url": "/static/js/main.dd51b18e.chunk.js"
+  },
+  {
+    "revision": "c7c118fe8bcfff125356",
+    "url": "/static/js/runtime~main.190b8f29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
